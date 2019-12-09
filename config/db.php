cd08d86952c611ac94544c0cc78dc96cda2ec1e4
Nico Jones <nico@aleph.engineering>
2019-12-09 11:22:00 -0500

    Remove DB config from repository

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=u715364900_chivocuate',
+    'username' => 'u715364900_chivocuate',
+    'password' => 'ChivoCuate123*',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
