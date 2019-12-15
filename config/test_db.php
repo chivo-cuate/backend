fcafa7404d7110883edd36481988ee72de27704a
Nico <nico@aleph.engineering>
2019-12-15 00:36:17 -0500

    Remove test_db from repo

--- config/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=u715364900_chivocuate',
-    'username' => 'u715364900_chivocuate',
-    'password' => 'ChivoCuate123*',
-    'charset' => 'utf8',
-];
