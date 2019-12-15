ce9347f8ec5b8d2e3509e503b1abc419f3f6a483
Nico <nico@aleph.engineering>
2019-12-15 00:41:06 -0500

    Remove db.php from repo

---
+++ config/test_db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u715364900_chivocuate',
+    'username' => 'u715364900_chivocuate',
+    'password' => 'ChivoCuate123*',
+    'charset' => 'utf8',
+];
