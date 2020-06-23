83d352c6d8b3be46a85a24422bb55d115fbf1f04
Felix Daniel <fdbatista@users.noreply.github.com>
2020-06-23 08:03:03 -0400

    Update migration

--- migrations/m190724_182149_create_order_status_table.php
+++ migrations/m190724_182149_create_order_status_table.php
@@ -11,18 +11,19 @@ class m190724_182149_create_order_status_table extends Migration {
      * {@inheritdoc}
      */
     public function safeUp() {
-        $this->createTable('{{%order_status}}', [
-            'id' => $this->primaryKey(),
+        $this->createTable('order_status', [
+            'id' => $this->integer()->notNull()->unique(),
             'name' => $this->string()->notNull()->unique(),
             'slug' => $this->string()->notNull()->unique(),
         ], app\utilities\MigrationHelper::getTableOptions($this->db->driverName));
+        $this->addPrimaryKey('pk_order_status', 'order_status', 'id');
     }
 
     /**
      * {@inheritdoc}
      */
     public function safeDown() {
-        $this->dropTable('{{%order_status}}');
+        $this->dropTable('order_status');
     }
 
 }
